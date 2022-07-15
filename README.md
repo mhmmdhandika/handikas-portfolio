14a00778ce317e181660af5829e132b54fe5c1f0
Muhamad Handika Sopian <muhammadhandika78@gmail.com>
2022-07-15 09:30:37 +0700

    add something in readme

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 1. [React Js](https://reactjs.org/)
 2. [Tailwind CSS](https://tailwindcss.com/)
-3. [Google Drive]()
-4. [Read More More](https://www.npmjs.com/package/read-more-more)
+3. [Google Drive](https://drive.google.com/)
+4. [React icons](https://react-icons.github.io/react-icons/)
+5. [Read More More](https://www.npmjs.com/package/read-more-more)
--- src/Projects.js
+++ src/Projects.js
@@ -20,7 +20,7 @@ export default function Projects() {
               <div className='p-6 rounded-2xl border-[6px] border-slate-200 bg-gray-200 transition duration-500  hover:shadow-xl hover:border-slate-100' key={index}>
                 <img src={image} alt={`${name}'s screenshoot`} className='mb-4 border rounded-lg' />
                 <h3 className='mb-4 mt-6 font-semibold text-primary text-2xl'>{name}</h3>
-                <div className='mb-4 text-sm text-primary'>{description.length >= 95 ? <AdvReadMoreMore text={description} linesToShow={2} checkFor={50} btnStyles={{ color: '#6b7280' }} /> : description}</div>
+                <div className='mb-4 text-sm text-slate-400'>{description.length >= 95 ? <AdvReadMoreMore text={description} linesToShow={2} checkFor={50} color='#1e293b' btnStyles={{ color: '#6b7280' }} /> : description}</div>
                 {published === '' ? (
                   <button className='block mb-4 text-sm text-slate-500 underline cursor-not-allowed' disabled>
                     Not yet published
