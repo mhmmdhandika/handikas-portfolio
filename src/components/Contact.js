eb05ed78d0698b4435c54d37fbcbbf6a155061db
Muhamad Handika Sopian <muhammadhandika78@gmail.com>
2022-07-24 14:04:27 +0700

    Add required state in Contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,8 +11,9 @@ export default function Contact() {
   const scriptURL = process.env.REACT_APP_CONTACT_GOOGLE_SCRIPT;
   const form = useRef(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [formState, setFormState] = useState(false);
 
-  const handleForm = e => {
+  const handleSubmit = e => {
     e.preventDefault();
     setIsLoading(true);
     fetch(scriptURL, { method: 'POST', body: new FormData(form.current) })
@@ -24,6 +25,7 @@ export default function Contact() {
           button: true,
           timer: 1500,
         });
+        setFormState(false);
         form.current.reset();
         console.log('Success!', response);
       })
@@ -43,25 +45,25 @@ export default function Contact() {
     <section id={thisSection.href} className='initial-section'>
       <div className='child-section'>
         <h2 className='title-section'>{thisSection.name} me</h2>
-        <form name='submit-to-google-sheet' className='py-6 text-primary-light sm:border-dashed sm:bg-white sm:border-slate-300 sm:border-4 sm:p-10 lg:px-16' onSubmit={handleForm} ref={form}>
+        <form name='submit-to-google-sheet' className='py-6 text-primary-light sm:border-dashed sm:bg-white sm:border-slate-300 sm:border-4 sm:p-10 lg:px-16' onSubmit={handleSubmit} ref={form}>
           <div>
             <label htmlFor='name' className='block'>
               Your name
             </label>
-            <input type='text' name='name' id='name' className='block' />
+            <input type='text' name='name' id='name' className='block' onChange={() => setFormState(true)} required />
           </div>
           <div className='mt-2'>
             <label htmlFor='email' className='block'>
               Email
             </label>
-            <input type='email' name='email' id='email' className='block peer invalid:border-pink-400 invalid:mb-1' />
-            <p className='hidden peer-invalid:block text-pink-600 text-sm mb-2'>Please provide a valid email address.</p>
+            <input type='email' name='email' id='email' className={`block peer ${formState === false ? '' : 'required:invalid:border-pink-400 invalid:mb-1'}`} onChange={() => setFormState(true)} required />
+            <p className={`hidden ${formState === false ? '' : 'peer-invalid:block text-pink-600 text-sm mb-2'}`}>Please provide a valid email address.</p>
           </div>
           <div className='mt-2'>
             <label htmlFor='message' className='block'>
               Message
             </label>
-            <textarea name='message' id='message' cols='30' rows='4' className='block'></textarea>
+            <textarea name='message' id='message' cols='30' rows='4' className='block' onChange={() => setFormState(true)} required></textarea>
           </div>
           <div className='flex justify-end'>
             <button type='submit' className={`mt-4 py-2 px-4 btn text-white font-semibold ${isLoading ? 'cursor-progress hover:bg-primary-light hover:text-white' : 'hover:text-primary-light'}`}>
