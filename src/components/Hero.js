85ef62c4e7c3a098bb304bdce7a91764a9ad790c
Muhamad Handika <muhammadhandika78@gmail.com>
2022-10-10 21:11:49 +0700

    Add active link in navbar

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,13 @@
-import { useContext } from "react";
-import { SectionContext } from "../App";
+import { useContext } from 'react';
+import { SectionContext } from '../App';
 import {
   AiOutlineGithub,
   AiOutlineInstagram,
   AiFillLinkedin,
-} from "react-icons/ai";
-import { FaQuora } from "react-icons/fa";
-import myImg from "../assets/img/my-vector.jpg";
-import socmedData from "../assets/json/socmed.json";
+} from 'react-icons/ai';
+import { FaQuora } from 'react-icons/fa';
+import myImg from '../assets/img/my-vector.jpg';
+import socmedData from '../assets/json/socmed.json';
 
 export default function Hero() {
   const thisSection = useContext(SectionContext).filter((section, index) => {
@@ -15,23 +15,23 @@ export default function Hero() {
   })[0];
 
   return (
-    <header id={thisSection.href} className="initial-section mb-44 md:mt-44">
-      <div className="child-section p-6 mx-auto xl:p-0 md:flex md:justify-between md:items-start lg:items-center">
-        <div className="text-primary sm:w-10/12 md:w-3/5">
-          <h4>Hello there👋 I am</h4>
-          <h1 className="mt-2 mb-3 text-6xl text-secondary font-bold">
+    <header id={thisSection.href} className='initial-section mb-44 md:mt-44'>
+      <div className='child-section p-6 mx-auto xl:p-0 md:flex md:justify-between md:items-start lg:items-center'>
+        <div className='text-primary sm:w-10/12 md:w-3/5'>
+          <h4>Hello there 👋 I am</h4>
+          <h1 className='mt-2 mb-3 text-6xl text-secondary font-bold'>
             Muhamad <br />
             Handika S.
           </h1>
-          <p className="py-4 text-md">
+          <p className='py-4 text-md'>
             Self-taught developer | Life-long learner
           </p>
-          <p className="md:text-sm">
+          <p className='md:text-sm'>
             A boy who interested in Frontend Web Development.
             <br /> Currently, I focus in React Js and kinda bit interested in
             UI/UX design.
           </p>
-          <ul className="flex">
+          <ul className='flex'>
             {socmedData.map((socmed, index) => {
               const { name, url } = socmed;
               const logo = [
@@ -43,7 +43,7 @@ export default function Hero() {
 
               return (
                 <li key={index}>
-                  <a href={url} target="blank" className="block socmed-icons">
+                  <a href={url} target='blank' className='block socmed-icons'>
                     {logo[index]}
                   </a>
                 </li>
@@ -51,11 +51,11 @@ export default function Hero() {
             })}
           </ul>
         </div>
-        <div className="hidden group md:block md:w-2/5">
+        <div className='hidden group md:block md:w-2/5'>
           <img
             src={myImg}
-            alt="anu"
-            className="rounded-full transition duration-500 group-hover:animate-bounce"
+            alt='anu'
+            className='rounded-full transition duration-500 group-hover:animate-bounce'
           />
         </div>
       </div>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -8,16 +8,21 @@ export default function Skills() {
   })[0];
 
   return (
-    <section id={thisSection.href} className='initial-section '>
+    <section id={thisSection.href} className='initial-section'>
       <div className='child-section'>
         <h2 className='title-section'>{thisSection.name}</h2>
         <ul className='grid gap-4 grid-cols-3 content-center md:gap-5 md:grid-cols-4 xl:grid-cols-5'>
           {skillsData.map((skill, index) => {
             return (
-              <li className='group relative p-3 border-2 border-slate-300 transition ease-in hover:border-[3px] hover:shadow-lg hover:-translate-y-2 md:p-6' key={index}>
+              <li
+                className='group relative p-3 border-2 border-slate-300 transition ease-in hover:border-[3px] hover:shadow-lg hover:-translate-y-2 md:p-6'
+                key={index}
+              >
                 <img src={skill.logo} alt={skill.name} />
                 <div className='opacity-0 transition duration-500 group-hover:block absolute bottom-0 left-1/2 -translate-x-1/2 w-full h-fit backdrop-blur-sm group-hover:opacity-100 group-hover:top-1/2 group-hover:-translate-y-1/2 group-hover:bg-slate-200 group-hover:bg-opacity-70 text-center'>
-                  <h4 className='font-semibold text-slate-700 transition duration-1000'>{skill.name}</h4>
+                  <h4 className='font-semibold text-slate-700 transition duration-1000'>
+                    {skill.name}
+                  </h4>
                 </div>
               </li>
             );
