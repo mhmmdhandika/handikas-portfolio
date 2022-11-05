3cf97de3e867f8df2fd41923748ccd83c2688816
Muhamad Handika <muhammadhandika78@gmail.com>
2022-11-05 14:54:19 +0700

    Add shorter link project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,50 +1,52 @@
-import { useContext } from "react";
-import { SectionContext } from "../App";
-import projectsData from "../assets/json/projects.json";
-import { AdvReadMoreMore } from "read-more-more";
+import { useContext } from 'react';
+import { SectionContext } from '../App';
+import projectsData from '../assets/json/projects.json';
+import { AdvReadMoreMore } from 'read-more-more';
 
 export default function Projects() {
   const thisSection = useContext(SectionContext).filter((section, index) => {
     return index === 1;
   })[0];
 
+  const lastFourProjects = -4;
+
   return (
-    <section id={thisSection.href} className="initial-section mt-60 mb-16">
-      <div className="child-section">
-        <h2 className="title-section">{`My ${thisSection.name}`}</h2>
-        <div className="grid gap-5 sm:grid-cols-2">
-          {projectsData.map((project, index) => {
+    <section id={thisSection.href} className='initial-section mt-60 mb-16'>
+      <div className='child-section'>
+        <h2 className='title-section'>{`My ${thisSection.name}`}</h2>
+        <div className='grid gap-5 sm:grid-cols-2'>
+          {projectsData.slice(lastFourProjects).map((project, index) => {
             const { name, image, description, repository, published } = project;
 
             return (
               <div
-                className="p-6 rounded-2xl border-4 border-slate-400 bg-slate-100 transition duration-500  hover:shadow-xl hover:scale-105"
+                className='p-6 rounded-2xl border-4 border-slate-400 bg-slate-100 transition duration-500  hover:shadow-xl hover:scale-105'
                 key={index}
               >
                 <img
                   src={image}
                   alt={`${name}'s screenshoot`}
-                  className="mb-4 rounded-lg"
+                  className='mb-4 rounded-lg'
                 />
-                <h3 className="mb-4 mt-6 font-semibold text-primary text-2xl">
+                <h3 className='mb-4 mt-6 font-semibold text-primary text-2xl'>
                   {name}
                 </h3>
-                <div className="mb-4 text-sm text-primary">
+                <div className='mb-4 text-sm text-primary'>
                   {description.length >= 95 ? (
                     <AdvReadMoreMore
                       text={description}
                       linesToShow={2}
                       checkFor={50}
-                      color="#1e293b"
-                      btnStyles={{ color: "#6b7280" }}
+                      color='#1e293b'
+                      btnStyles={{ color: '#6b7280' }}
                     />
                   ) : (
                     description
                   )}
                 </div>
-                {published === "" ? (
+                {published === '' ? (
                   <button
-                    className="block mb-4 text-sm text-slate-500 underline cursor-not-allowed"
+                    className='block mb-4 text-sm text-slate-500 underline cursor-not-allowed'
                     disabled
                   >
                     Not yet published
@@ -52,17 +54,17 @@ export default function Projects() {
                 ) : (
                   <a
                     href={published}
-                    target="blank"
-                    className="block mb-4 text-sm text-primary underline"
+                    target='blank'
+                    className='block mb-4 text-sm text-primary underline'
                   >
                     Website published
                   </a>
                 )}
-                <button className="btn group">
+                <button className='btn group'>
                   <a
                     href={repository}
-                    target="blank"
-                    className="font-semibold text-sm text-white group-hover:text-primary"
+                    target='blank'
+                    className='font-semibold text-sm text-white group-hover:text-primary'
                   >
                     Go to repository
                   </a>
