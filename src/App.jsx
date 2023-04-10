24a0424d7b3c973aedda6593e5b468c4ed57414c
mhmmdhandika <muhammadhandika78@gmail.com>
2023-04-10 13:16:43 +0700

    set back to lazy components

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { createContext, Suspense } from 'react';
+import { createContext, Suspense, lazy } from 'react';
 import Loading from './components/Loading';
 import sectionList from './data/section-list.json';
 
-import Navbar from './partials/Navbar';
-import Hero from './partials/Hero';
-import Projects from './partials/Projects';
-import Skills from './partials/Skills';
-import Contact from './partials/Contact';
-import Footer from './partials/Footer';
+// import Navbar from './partials/Navbar';
+// import Hero from './partials/Hero';
+// import Projects from './partials/Projects';
+// import Skills from './partials/Skills';
+// import Contact from './partials/Contact';
+// import Footer from './partials/Footer';
 
-// const Navbar = lazy(() => import('./partials/Navbar'));
-// const Hero = lazy(() => import('./partials/Hero'));
-// const Projects = lazy(() => import('./partials/Projects'));
-// const Skills = lazy(() => import('./partials/Skills'));
-// const Contact = lazy(() => import('./partials/Contact'));
-// const Footer = lazy(() => import('./partials/Footer'));
+const Navbar = lazy(() => import('./partials/Navbar'));
+const Hero = lazy(() => import('./partials/Hero'));
+const Projects = lazy(() => import('./partials/Projects'));
+const Skills = lazy(() => import('./partials/Skills'));
+const Contact = lazy(() => import('./partials/Contact'));
+const Footer = lazy(() => import('./partials/Footer'));
 
 const SectionContext = createContext(null);
 
