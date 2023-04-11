94920d17714166d0acab3f69d59dd1ea209c0e48
mhmmdhandika <muhammadhandika78@gmail.com>
2023-04-11 13:09:39 +0700

    cchange year of footer to dynamic ne

--- src/partials/Footer.jsx
+++ src/partials/Footer.jsx
@@ -1,24 +1,26 @@
-import { IoLocationSharp } from "react-icons/io5";
+import { IoLocationSharp } from 'react-icons/io5';
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer>
-      <div className="child-section py-11 text-center md:py-20">
-        <p className="text-md mt-3 font-semibold mx-auto md:max-w-[380px]">
+      <div className='child-section py-11 text-center md:py-20'>
+        <p className='text-md mx-auto mt-3 font-semibold md:max-w-[380px]'>
           <q>
             {`Hi folks! You're the reason I'm smiling today, glad I'm you have
             come to my website :)`}
           </q>
         </p>
-        <div className="mt-3 text-sm flex justify-center items-center">
-          <span className="mr-1">
+        <div className='mt-3 flex items-center justify-center text-sm'>
+          <span className='mr-1'>
             <IoLocationSharp />
           </span>
           <p>Bekasi, Indonesia</p>
         </div>
       </div>
-      <div className="w-full py-2 bg-slate-50 text-center text-slate-700 text-xs font-semibold">
-        Copyright © 2022. All rights reserved.
+      <div className='w-full bg-slate-50 py-2 text-center text-xs font-semibold text-slate-700'>
+        Copyright © {currentYear}. All rights reserved.
       </div>
     </footer>
   );
